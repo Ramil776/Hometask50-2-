435fe2170cbc8f7344221a1cb1605ea37bad2e50
Ramil776 <ramil.sadigov00@mail.ru>
2024-09-06 13:18:06 +0400

    Hometask50(2)

---
+++ app.js
@@ -0,0 +1,5 @@
+document.querySelector('h1').textContent = 'Series';
+let primaryElements = document.querySelectorAll('.primary');
+primaryElements.forEach(function(element) {
+    element.style.backgroundColor = 'teal';
+});
